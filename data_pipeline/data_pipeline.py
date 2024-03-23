dbe49045722e623cc3c0b328dad9cdcabbf8d347
Justin Lee <2025jlee@tjhsst.edu>
2024-03-23 18:19:06 -0400

    add get all labels

--- data_pipeline/data_pipeline.py
+++ data_pipeline/data_pipeline.py
@@ -1,14 +1,17 @@
 from pathlib import Path
+import pandas as pd
 import gzip
 
-MIMIC_PATH = "./data/mimiciv/2.2"
 
+DATA_PATH = "./data"
+MIMIC_PATH = f"{DATA_PATH}/mimiciv/2.2"
+PROCESSED_DATA_PATH = "./processed_data"
 
 def unzip_all_data() -> None:
     for file in Path(MIMIC_PATH).glob("**/*"):
         if file.is_file() and file.suffixes == [".csv", ".gz"]:
             directory = file.parent
-            new_directory = Path("./processed_data") / directory.relative_to("./data")
+            new_directory = Path(PROCESSED_DATA_PATH) / directory.relative_to(DATA_PATH)
             new_file = new_directory / file.with_suffix("").name
 
             if new_file.exists():
@@ -20,5 +23,16 @@ def unzip_all_data() -> None:
                 with open(new_file, "wb+") as f:
                     f.write(file_content)
 
+def get_all_labels() -> "dict[str, list[str]]":
+    labels = {}
+    for file in Path(PROCESSED_DATA_PATH).glob("**/*"):
+        if file.is_file() and file.suffix == ".csv":
+            df = pd.read_csv(file)
+            name = file.with_suffix("").name
+            labels[name] = [c for c in df.columns]
+    
+    return labels
+
 
 unzip_all_data()
+print(get_all_labels())
