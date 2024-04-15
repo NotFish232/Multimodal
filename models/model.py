5852d6656a009ea3c587756de9eab7f5f0bcde19
Justin Lee <2025jlee@tjhsst.edu>
2024-04-14 21:41:56 -0400

    add dropout to ftc

--- models/model.py
+++ models/model.py
@@ -10,6 +10,7 @@ class MimicCXRModel(nn.Module):
         super().__init__()
 
         self.a_fn = nn.LeakyReLU()
+        self.drop = nn.Dropout(0.5)
 
         self.conv_net = ConvNextV2Model.from_pretrained(
             "facebook/convnextv2-base-22k-384"
@@ -31,7 +32,7 @@ class MimicCXRModel(nn.Module):
         num_images: T.Tensor,
     ) -> T.Tensor:
         image_features = self.conv_net(images).pooler_output
-        image_features = self.a_fn(self.ftc1(image_features))
+        image_features = self.drop(self.a_fn(self.ftc1(image_features)))
 
         batched_features_list = []
         images_idx = 0
@@ -46,7 +47,7 @@ class MimicCXRModel(nn.Module):
             images_idx += num_image
 
         batched_image_features = T.concat(batched_features_list)
-        batched_image_features = self.a_fn(self.ftc2(batched_image_features))
+        batched_image_features = self.drop(self.a_fn(self.ftc2(batched_image_features)))
         batched_image_features = batched_image_features.to(T.int64)
 
         complete_embeddings = T.concat((tokens, batched_image_features), dim=1)
@@ -56,8 +57,8 @@ class MimicCXRModel(nn.Module):
             complete_embeddings, attention_mask=attention_mask
         ).last_hidden_state
 
-        final_output = self.a_fn(self.ftc3(transformer_output))
-        final_output = self.a_fn(self.ftc4(final_output))
+        final_output = self.drop(self.a_fn(self.ftc3(transformer_output)))
+        final_output = self.drop(self.a_fn(self.ftc4(final_output)))
         final_output = self.ftc5(final_output)
 
         return final_output
